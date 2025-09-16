7f822c1934d8cf2fdf3b1fdfcba6009b371ee580
LuuHoangAnh <luuhoanganh3122003@gmail.com>
2025-09-16 17:28:14 +0700

    1.Create API CRUD (Create, Read, Update, Delete) 2.Exception & Validation 3.Lombok

---
+++ src/main/java/com/example/identity_service/exception/ErrorCode.java
@@ -0,0 +1,26 @@
+package com.example.identity_service.exception;
+
+public enum ErrorCode {
+    UNCATEGORIZEO_EXCEPTION(9999, "Uncategoried error"),
+    INVALID_KEY(1001, "Invalid message key"),
+    USER_EXISTED(1002, "User existed"),
+    USERNAME_INVALID(1003, "Username must be at lest 3 characters"),
+    PASSWORD_INVALID(1004, "Password must be at lest 8 characters")
+    ;
+
+    ErrorCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    private int code;
+    private String message;
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
