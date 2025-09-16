acf50a82263cb622d6b29ddbcefee71a8f9de571
LuuHoangAnh <luuhoanganh3122003@gmail.com>
2025-09-16 18:40:25 +0700

    1.Refactor code 2.Create dto.UserResponse so that the service layer returns data to the controller layer, it will not return an entity but will return another entity because there will be a need to change certain objects

---
+++ src/main/java/com/example/identity_service/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.example.identity_service.mapper;
+
+import com.example.identity_service.dto.request.UserCreationRequest;
+import com.example.identity_service.dto.request.UserUpdateRequest;
+import com.example.identity_service.dto.request.response.UserResponse;
+import com.example.identity_service.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+     User toUser(UserCreationRequest request);
+    //o day du lieu nhan ve 1 parameter la request theo kieu UserCreationRequest va se tra ve 1 class la User
+    UserResponse toUserResponse(User user);
+    //Map tu User ve UserResponse
+     void updateUser(@MappingTarget User user, UserUpdateRequest request);
+     //map cac data tu UserUpdateRequest vao User
+}
